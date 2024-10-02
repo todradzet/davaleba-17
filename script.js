c6ba02f7c4faa6ee159c8b7af303ea6d4faa974c
todradzet <tamta.todradze.1@iliauni.edu.ge>
2024-10-02 20:49:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function compareWordLengths(word1, word2) {
+    
+    return word1.length === word2.length;
+}
+console.log(compareWordLengths("apple", "grape"));  
+console.log(compareWordLengths("apple", "banana")); 
